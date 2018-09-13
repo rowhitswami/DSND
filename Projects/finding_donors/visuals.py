d4a7f6df7c24159aaae38be1edada09b8817ce9c
Rohit Swami <rowhitswami1@gmail.com>
2018-09-14 00:14:19 +0530

    Change bar_width

--- Projects/finding_donors/visuals.py
+++ Projects/finding_donors/visuals.py
@@ -63,7 +63,7 @@ def evaluate(results, accuracy, f1):
     fig, ax = pl.subplots(2, 3, figsize = (11,7))
 
     # Constants
-    bar_width = 0.5
+    bar_width = 0.3
     colors = ['#A00000','#00A0A0','#00A000']
     
     # Super loop to plot four panels of data
